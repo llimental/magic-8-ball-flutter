cad92a4a29c55e64fdea49da13449a8fb36b89ff
ideal_lust3r <45708630+llimental@users.noreply.github.com>
2022-01-22 00:39:12 +0900

    Requirement_5
    Update the State(setState)

--- lib/main.dart
+++ lib/main.dart
@@ -33,10 +33,11 @@ class _BallState extends State<Ball> {
     return Center(
       child: TextButton(
         onPressed: (){
-            ballNumber = Random().nextInt(5);
-            print('$ballNumber');
+          setState(() {
+            ballNumber = Random().nextInt(5)+1;
+          });
         },
-        child: Image.asset('images/ball1.png'),
+        child: Image.asset('images/ball$ballNumber.png'),
       ),
     );
   }
